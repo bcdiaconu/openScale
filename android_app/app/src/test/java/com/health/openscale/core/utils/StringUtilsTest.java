372b158fc3c65e8904b2b9a62f0856e79c42b94a
Diaconu Cosmin <bcdiaconu@gmail.com>
2021-08-28 12:30:22 +0300

    improved StringUtilsTest

--- android_app/app/src/test/java/com/health/openscale/core/utils/StringUtilsTest.java
+++ android_app/app/src/test/java/com/health/openscale/core/utils/StringUtilsTest.java
@@ -22,6 +22,12 @@ public class StringUtilsTest {
         stringTest = new String(" ");
         assertTrue(StringUtils.isNullOrWhitespace(stringTest));
 
+        stringTest = new String(" \t\r\n");
+        assertTrue(StringUtils.isNullOrWhitespace(stringTest));
+
+        stringTest = null;
+        assertTrue(StringUtils.isNullOrWhitespace(stringTest));
+
         stringTest = new String("\n\t\r    \t");
         assertTrue(StringUtils.isNullOrWhitespace(stringTest));
 
@@ -38,28 +44,43 @@ public class StringUtilsTest {
     @Test
     public void splitByWhitespace() {
         String stringTest = new String("\n\t\r    \t1 2\t\t\n3");
-        String[] stringsSplitted =StringUtils.splitByWhitespace(stringTest);
-
+        String[] stringsSplitted = StringUtils.splitByWhitespace(stringTest);
         String[] expected = new String[]{"1", "2", "3"};
-        assertArrayEquals(expected,stringsSplitted);
+        assertArrayEquals(expected, stringsSplitted);
 
         stringTest = new String("\n\t\r    \t1 2\t\t\n3 \t\n\r");
-        stringsSplitted =StringUtils.splitByWhitespace(stringTest);
-
+        stringsSplitted = StringUtils.splitByWhitespace(stringTest);
         expected = new String[]{"1", "2", "3"};
-        assertArrayEquals(expected,stringsSplitted);
+        assertArrayEquals(expected, stringsSplitted);
 
         stringTest = new String("1 2\t\t\n3 \t\n\r");
-        stringsSplitted =StringUtils.splitByWhitespace(stringTest);
-
+        stringsSplitted = StringUtils.splitByWhitespace(stringTest);
         expected = new String[]{"1", "2", "3"};
-        assertArrayEquals(expected,stringsSplitted);
+        assertArrayEquals(expected, stringsSplitted);
+
+        stringTest = null;
+        stringsSplitted = StringUtils.splitByWhitespace(stringTest);
+        expected = null;
+        assertArrayEquals(expected, stringsSplitted);
+
+        stringTest = new String("\t\t\n \t\n\r");
+        stringsSplitted = StringUtils.splitByWhitespace(stringTest);
+        expected = null;
+        assertArrayEquals(expected, stringsSplitted);
     }
 
     @Test
     public void generateStringWithRepeatingChar() {
-        String testString = StringUtils.generateStringWithRepeatingChar(6);
-        String expected = "      ";
+        String testString = StringUtils.generateStringWithRepeatingChar(-1);
+        String expected = "";
+        assertEquals(expected, testString);
+
+        testString = StringUtils.generateStringWithRepeatingChar(0);
+        expected = "";
+        assertEquals(expected, testString);
+
+        testString = StringUtils.generateStringWithRepeatingChar(6);
+        expected = "      ";
         assertEquals(expected, testString);
 
         testString = StringUtils.generateStringWithRepeatingChar(4, 'c');
